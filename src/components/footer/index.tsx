58c3e8cd83d95ec2b8cf6545bb138d90c333bc64
szumi112 <mattszumilo@gmail.com>
2024-08-28 22:13:31 +0200

    chore: set up components folder for further development (#3)
    * chore: set up components folder for further development
    
    * chore: move the native html component into custom elements
    
    * chore: move repetitive components to layout
    
    ---------
    
    Co-authored-by: Yi Tam <tam.yi3d@gmail.com>

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <div>Footer</div>
+  </footer>
+    };
+
+    export default Footer;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header><div>Header</div></header>;
+};
+
+export default Header;
