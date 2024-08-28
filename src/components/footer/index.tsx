b84bc22417df1b14d95fadde302aa6c1a79f4356
Yi Tam <tam.yi3d@gmail.com>
2024-08-28 22:12:39 +0200

    chore: move the native html component into custom elements

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
-};
+  return <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <div>Footer</div>
+  </footer>
+    };
 
-export default Footer;
+    export default Footer;
