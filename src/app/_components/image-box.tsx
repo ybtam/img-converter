5da0b788886ffbd842eb47f0b2f7a90aa50fae29
Yi Tam <tam.yi3d@gmail.com>
2024-09-13 20:23:56 +0200

    feat: move transition logic to box component to handle multiple converts at the same time

--- src/app/_components/image-box.tsx
+++ src/app/_components/image-box.tsx
@@ -1,25 +1,36 @@
+import {useEffect, useState, useTransition} from "react";
+import {convertImage} from "@/utils/imageConverter";
+
 interface Props {
-  imageUrl: string | undefined
-  isPending: boolean
-  originalFileName?: string
-  format?: 'webp' | 'jpeg' | 'png'
+  format: 'webp' | 'jpeg' | 'png'
+  file: File | null
+  quality: number
 }
 
-export default function ImageBox({ imageUrl, isPending}: Props) {
+export default function ImageBox({ file, quality, format }: Props) {
+  const [imageUrl, setImageUrl] = useState<string>()
+
+  const [isPending, startTransition] = useTransition()
+
+  useEffect(() => {
+    if (!file) return
+
+    startTransition(() => convertImage({file, callback:setImageUrl, quality, format}))
+  }, [quality, file]);
+
   return (
     <div className="mt-2 flex flex-col items-center">
       {
-        !imageUrl ?
-          <p className="text-center">No image to show</p>
-          :
         isPending ?
           <p className="text-center">Converting...</p>
-          : (<>
+          : ( !imageUrl ?
+            <p className="text-center">{format}</p>
+            :<>
               <p className="mb-2 text-lg">File converted</p>
               <div className="flex w-80 items-center justify-between rounded-sm bg-white p-3 shadow-sm sm:w-[413px]">
                 <div className="flex gap-2">
-                  <img src={imageUrl} alt="converted image" className="w-16 h-16 rounded-full"/>
-                  {imageUrl?.split('/').pop()}
+                  <img src={imageUrl} alt="converted image" className="w-16 h-16"/>
+                  {imageUrl?.split('/').pop()} ({format})
                 </div>
                 <a download={imageUrl?.split('/').pop()} href={imageUrl}>
                   <button
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,29 +9,16 @@ import ImageBox from "@/app/_components/image-box";
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null)
-  const [convertedFile, setConvertedFile] = useState<string>()
-  const [isPending, startTransition] = useTransition()
   const [quality, setQuality] = useState(0.85)
-
-  const handleFileAccepted = (acceptedFile: File) => {
-    setFile(acceptedFile)
-    const fileNameWithoutExtension = acceptedFile.name.replace(/\.[^/.]+$/, '')
-    startTransition(() => convertImage({file:acceptedFile, callback:setConvertedFile, quality, format: 'webp'}))
-  }
-
-  const handleQualityChange = (newQuality: number) => {
-    setQuality(newQuality)
-    if (file) {
-      startTransition(() => convertImage({file, callback:setConvertedFile, quality:newQuality, format: 'webp'}))
-    }
-  }
 
   return (
     <div className="flex flex-col items-center justify-center gap-6">
       <h1 className="text-xl">Convert your image</h1>
-      <QualitySlider quality={quality} onQualityChange={handleQualityChange} />
-      <Dropzone onFileAccepted={handleFileAccepted} />
-      <ImageBox imageUrl={convertedFile} isPending={isPending} />
+      <QualitySlider quality={quality} onQualityChange={setQuality} />
+      <Dropzone onFileAccepted={setFile} />
+      <ImageBox file={file} quality={quality} format={'webp'} />
+      <ImageBox file={file} quality={quality} format={'jpeg'} />
+      <ImageBox file={file} quality={quality} format={'png'} />
     </div>
   )
 }
