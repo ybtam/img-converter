d38c31e3e485f4158ed50fa6dcf89c1da18de6c3
Yi Tam <tam.yi3d@gmail.com>
2024-09-14 10:50:40 +0200

    fix: show pending state

--- src/app/_components/image-box.tsx
+++ src/app/_components/image-box.tsx
@@ -1,46 +1,52 @@
-import {useEffect, useState, useTransition} from "react";
-import {convertImage} from "@/utils/imageConverter";
+import { useEffect, useState, useTransition } from "react";
+import { convertImage } from "@/utils/imageConverter";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 interface Props {
-  format: 'webp' | 'jpeg' | 'png'
-  file: File | null
-  quality: number
+  format: 'webp' | 'jpeg' | 'png';
+  file: File | null;
+  quality: number;
 }
 
 export default function ImageBox({ file, quality, format }: Props) {
-  const [imageUrl, setImageUrl] = useState<string>()
-
-  const [isPending, startTransition] = useTransition()
+  const [imageUrl, setImageUrl] = useState<string>();
+  const [isPending, setIsPending] = useState(false);
 
   useEffect(() => {
-    if (!file) return
+    if (!file) return;
 
-    startTransition(() => convertImage({file, callback:setImageUrl, quality, format}))
+    setIsPending(true);
+    convertImage({
+      file,
+      callback: url => {
+        setImageUrl(url);
+        setIsPending(false);
+      },
+      quality,
+      format,
+    })
   }, [quality, file]);
 
   return (
     <div className="mt-2 flex flex-col items-center">
-      {
-        isPending ?
-          <p className="text-center">Converting...</p>
-          : ( !imageUrl ?
-            <p className="text-center">{format}</p>
-            :<>
-              <p className="mb-2 text-lg">File converted</p>
-              <div className="flex w-80 items-center justify-between rounded-sm bg-white p-3 shadow-sm sm:w-[413px]">
-                <div className="flex gap-2">
-                  <img src={imageUrl} alt="converted image" className="w-16 h-16"/>
-                  {imageUrl?.split('/').pop()} ({format})
-                </div>
-                <a download={imageUrl?.split('/').pop()} href={imageUrl}>
-                  <button
-                    className="duration-10 rounded-sm bg-blue-500 px-4 py-1 text-white shadow-md transition delay-0 ease-in-out hover:scale-105 hover:bg-blue-600">
-                    Download
-                  </button>
-                </a>
-              </div>
-            </>)
-      }
+      {isPending ? (
+        <p className="text-center">Converting...</p>
+      ) : !imageUrl ? (
+        <p className="text-center">{format}</p>
+      ) : (
+        <div className="flex w-80 items-center justify-between rounded-sm bg-white p-3 shadow-sm sm:w-[413px]">
+          <div className="flex gap-2">
+            <img src={imageUrl} alt="converted image" className="w-16 h-16" />
+            {file?.name?.split(".").reverse().pop()} ({format})
+          </div>
+          <Button asChild>
+            <Link href={imageUrl} download={file?.name}>
+              Download
+            </Link>
+          </Button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
